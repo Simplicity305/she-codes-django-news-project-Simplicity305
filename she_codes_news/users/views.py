c3837b7c29f58b37e1a7f5d7535552b69885f41e
Simplicity305 <113986047+Simplicity305@users.noreply.github.com>
2022-12-18 00:20:47 +0800

    Created profile page & anchor link to lead to author's profile
    - Created userProfile.html
    - Added url for user profile
    - Created view for user profile
    - Added anchor link to take user to author's profile upon clicking author's name in story.html

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -1,8 +1,10 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+
 from django.views import generic
 from .models import CustomUser
 from .forms import CustomUserCreationForm
+from django.shortcuts import get_object_or_404
 
 class CreateAccountView(CreateView):
     form_class = CustomUserCreationForm
@@ -10,6 +12,15 @@ class CreateAccountView(CreateView):
     template_name = 'users/createAccount.html'
 
 
+class UserProfileView(generic.DetailView):
+    model = CustomUser
+    template_name = 'users/userProfile.html'
+    context_object_name = 'userProfile'
+    
+    def get_object(self, queryset=None): #TODO understand queryset a bit more
+        return get_object_or_404(CustomUser, username=self.kwargs['author_username'])
 
+    #calls CustomUser.objects.get based on that username 
+    #eg queryset, subset all users that have an id greater than 10 
 
-
+        return CustomUser.objects.filter(username__contains="wen").first()
